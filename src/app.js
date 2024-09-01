27b1b176a712c1cac767a6b1bff1c3127cb5158b
Juanmanms <juanmanms@gmail.com>
2024-09-01 07:42:53 +0200

    acceso a fontetes

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const attributeRoutes = require('./routes/attributes');
 app.use(express.json());
 
 const corsOptions = {
-    origin: ['http://localhost:5173', [/^https:\/\/torreblanca-front\.vercel\.app/]],
+    origin: ['http://localhost:5173', [/^https:\/\/torreblanca-front\.vercel\.app/], [/^https:\/\/fontetes-front\.vercel\.app/]],
     optionsSuccessStatus: 200
 };
 
