0ccd799ff745e94c7a46a753e1757e17ec28f0f6
juanmanms <juanmanms@gmail.com>
2026-05-12 16:07:00 +0200

    fix: actualizar la configuración de puertos y orígenes permitidos en docker-compose y app.js

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 
 const allowedOrigins = [
     'http://localhost:5173',
+    'http://127.0.0.1:5173',
     'https://torreblanca-front.vercel.app',
     'https://fontetes-front.vercel.app',
     'https://serraparera-front.vercel.app',
